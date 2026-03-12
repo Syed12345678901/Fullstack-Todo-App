c5aaa768c9f5e8b7a67d82aeb0acfc66276c3753
Syed Afnan <syedafnan9941@gmail.com>
2026-03-12 16:26:52 +0530

    second commit with env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.REACT_APP_API_URL,
+  // baseURL: "http://127.0.0.1:8000",
 });
 
 API.interceptors.request.use((config) => {
